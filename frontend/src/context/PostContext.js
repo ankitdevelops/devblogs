d512656e43baab7e7cc7874cf4abb292cc94331e
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-27 16:41:35 +0530

    .

--- frontend/src/context/PostContext.js
+++ frontend/src/context/PostContext.js
@@ -23,7 +23,7 @@ export const PostProvider = ({ children }) => {
   // get all posts
 
   const getPosts = async () => {
-    const url = "/api/blogs/";
+    const url = "/api/blogs";
     axios
       .get(url)
       .then((response) => {

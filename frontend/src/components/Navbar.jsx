544982657c8db5e5314212a62fc26dee2023fcaa
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-10 00:17:54 +0530

    added a temporary mobile navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,17 +2,22 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import PostContext from "../context/PostContext";
+import Categories from "./Categories";
 
 const Navbar = () => {
   const { isLoggedin, userInfo } = useContext(AuthContext);
   const { searchPost } = useContext(PostContext);
   const [showDropdown, setShowDropdown] = useState(false);
-
+  const [showMobileNav, setshowMobileNav] = useState(false);
   const [keyword, setKeyword] = useState("");
   const navigate = useNavigate();
 
   function myFunction() {
     setShowDropdown(!showDropdown);
+  }
+
+  function mobileNav() {
+    setshowMobileNav(!showMobileNav);
   }
 
   const signOut = () => {
@@ -35,7 +40,7 @@ const Navbar = () => {
             <div className="navbar-brand">
               <Link to="/">Dev</Link>
             </div>
-            <div className="search">
+            <div className="search d-none">
               <form className="form-container" onSubmit={handleSearch}>
                 <input
                   type="search"
@@ -67,7 +72,26 @@ const Navbar = () => {
                 <Link to="/signin">Sign in</Link>
               )}
             </div>
-            <div className="profile">
+            <button
+              type="submit"
+              className="search-btn d-sm-none"
+              onClick={mobileNav}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-list"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                />
+              </svg>
+            </button>
+            <div className="profile d-none d-md-block">
               <div>
                 {isLoggedin && (
                   <img
@@ -115,6 +139,86 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+      </div>
+      {/* mobile nav */}
+      <div className={showMobileNav ? `nav__mobile  ` : "d-none d-sm-none"}>
+        <button
+          type="submit"
+          className="search-btn d-sm-none position-absolute top-0 right-0 float-end"
+          onClick={mobileNav}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            className="bi bi-x-lg"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+          </svg>
+        </button>
+        {/* search */}
+        <div className="search w-100 text-center">
+          <form className="form-container" onSubmit={handleSearch}>
+            <input
+              type="search"
+              placeholder="Search"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+              className="w-100"
+            />
+            <button type="submit" className="search-btn">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </button>
+          </form>
+        </div>
+        <hr />
+        <ul className="my-2 fs-2 list-unstyled text-center">
+          {userInfo.username && (
+            <>
+              <h3 className="text-decoration-underline">Links</h3>
+              <li>
+                <Link
+                  className="btn  border-0 btn-outline-light"
+                  to={`/profile/${userInfo.username}`}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <Link className="btn  border-0 btn-outline-light" to="/create">
+                  Create{" "}
+                </Link>
+              </li>
+              <li>
+                <Link className="btn  border-0 btn-outline-light" to="/saved">
+                  Reading List
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="btn  border-0 btn-outline-light"
+                  to="/dashboard"
+                >
+                  Dashboard
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+        <hr />
+        <h3 className="text-decoration-underline">Categories</h3>
+        <Categories />
       </div>
     </nav>
   );
